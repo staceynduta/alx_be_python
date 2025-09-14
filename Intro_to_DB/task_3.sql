9d495d625abc6a9f58b86aebcda60f3805c7c027
Stacey Nduta <staceyndutaconsulting@gmail.com>
2025-09-14 21:53:26 +0300

    update

--- Intro_to_DB/task_3.sql
+++
@@ -1,5 +0,0 @@
--- Use the database to create tables within it.
-USE alx_book_store;
-
--- List all tables in the current database.
-SHOW TABLES;
--- Intro_to_DB/task_5.sql
+++
@@ -1,4 +0,0 @@
--- Insert a single row into the Customers table.
--- The database name 'alx_book_store' will be passed as a MySQL command argument.
-INSERT INTO customer (customer_id, customer_name, email, address)
-VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
